38ed39892d0263e9b4374b835c8bee202239b49f
GanangPrakoso <ganangmanggolo@gmail.com>
2020-02-17 13:42:51 +0700

    initial commit

---
+++ models/user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+module.exports = (sequelize, DataTypes) => {
+  class User extends sequelize.Sequelize.Model {
+    static associate (models) {
+
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: `name must be filled`
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: `email must be filled`
+        },
+        isEmail: {
+          args: true,
+          msg: `email must be according to email format`
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: `name must be filled`
+        },
+        len: {
+          args: [5],
+          msg: `password require 5 characters minimum`
+        }
+      }
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {});
+
+  return User;
+};
