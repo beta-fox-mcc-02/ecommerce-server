0cc5a247b67e5226de8d59ad2d7426a2a6e3d91b
Heri Susanto <susantoh41@gmail.com>
2020-02-17 17:31:33 +0700

    Adding products test suite

---
+++ tests/product.test.js
@@ -0,0 +1,103 @@
+const request = require('supertest')
+const app = require('../index')
+
+describe('Product Endpoints', () => {
+    it('should create a new post', done => {
+        request(app)
+            .post('/products')
+            .send({
+                name: 'Surface Studio',
+                image_url: 'https://www.testimage.com/surface.jpg',
+                price: 30000000,
+                stock: 20
+            })
+            .then(res => {
+                expect(res.statusCode).toEqual(201)
+                expect(res.body).toHaveProperty('product')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('should fetch a single product', done => {
+        const productId = 1
+        request(app)
+            .get(`/products/${productId}`)
+            .then(res => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body).toHaveProperty('product')
+                done()
+            })
+            .catch(done)
+    })
+
+    it('should fetch all products', async () => {
+        request(app)
+            .get('/products')
+            .then(res => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body).toHaveProperty('products')
+                expect(res.body.products).toHaveLength(1)
+                done()
+            })
+            .catch(done)
+    })
+
+    it('should update a product', done => {
+        const productId = 1
+        request(app)
+            .put(`/products/${productId}`)
+            .send({
+                name: 'Surface Studio 2',
+                image_url: '',
+                price: 35000000,
+                stock: 19
+            })
+            .then(res => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body).toHaveProperty('product')
+                expect(res.body.post).toHaveProperty(
+                    'name',
+                    'image_url',
+                    'price',
+                    'stock'
+                )
+                done()
+            })
+            .catch(done)
+    })
+
+    it('should return status code 400 if db constraint is violated', done => {
+        request(app)
+            .post('/products')
+            .send({
+                name: 'Surface Studio 2'
+            })
+            .then(res => {
+                expect(res.statusCode).toEqual(400)
+                expect(res.body).toHaveProperty('errors')
+            })
+            .catch(done)
+    })
+
+    it('should delete a product', done => {
+        const productId = 1
+        request(app)
+            .delete(`products/${productId}`)
+            .then(res => {
+                expect(res.statusCode).toEqual(204)
+                done()
+            })
+            .catch(done)
+    })
+
+    it('should respond with status code 404 if resource is not found', done => {
+        const productId = 1
+        request(app)
+            .get(`/products/${productId}`)
+            .then(res => {
+                expect(res.statusCode).toEqual(404)
+            })
+            .catch(done)
+    })
+})
