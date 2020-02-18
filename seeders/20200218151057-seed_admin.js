bcb250d9254fcd00c6b56ff1159e3e9b5e48fbcd
GanangPrakoso <ganangmanggolo@gmail.com>
2020-02-18 22:25:55 +0700

    env-template lupa. API doc menyusul

---
+++ seeders/20200218151057-seed_admin.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+        return queryInterface.bulkInsert('Users', [{
+          name: `admin`,
+          email: `admin@admin.com`,
+          password: `12345`,
+          isAdmin: true
+        }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+     return queryInterface.bulkDelete('Users', null, {});
+  }
+};
