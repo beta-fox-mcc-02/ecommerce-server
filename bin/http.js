a0ac2264099d897cee8a47b4d82aae88617ca55e
andrumahardi <andrumahardi77@gmail.com>
2020-02-20 06:01:39 +0700

    cors setting

--- bin/http.js
+++ bin/http.js
@@ -1,4 +1,6 @@
 const app = require('../app.js')
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.listen (PORT, () => console.log(`LISTENING ON PORT ${PORT}`))
