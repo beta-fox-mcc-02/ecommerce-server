2e1bf46105b7908eb96c05e8bd5b50554b4c84f2
andrumahardi <andrumahardi77@gmail.com>
2020-02-20 04:20:53 +0700

    second commit

--- bin/http.js
+++ bin/http.js
@@ -1,4 +1,4 @@
 const app = require('../app.js')
-const port = 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`LISTENING ON PORT ${port}`))
+app.listen (PORT, () => console.log(`LISTENING ON PORT ${PORT}`))
