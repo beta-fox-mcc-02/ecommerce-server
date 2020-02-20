6ecf4268cb96170fd88d66dbdf8c06c63e35cab3
andrumahardi <andrumahardi77@gmail.com>
2020-02-20 13:59:23 +0700

    deploy

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const cors = require('cors')
 //   preflightContinue: true,
 //   optionsSuccessStatus: 204
 // }
-
-app.use(cors())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
+
+app.use(cors())
 
 app.use(indexRouter)
 app.use(errHandler)
