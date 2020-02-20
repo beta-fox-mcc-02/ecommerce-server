e4122d2eca473cbf6790a3887a1e77a0b0ac0e0a
andrumahardi <andrumahardi77@gmail.com>
2020-02-20 08:45:07 +0700

    deploy

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const indexRouter = require('./routes/index.js')
 const errHandler = require('./middlewares/errHandler.js')
 const cors = require('cors')
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://ecommerce-cmsv1.firebaseapp.com',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
