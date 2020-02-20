f283195bdf03bf53dcf3d0c7d67d841b19dc093e
andrumahardi <andrumahardi77@gmail.com>
2020-02-20 08:40:56 +0700

    deploy

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,7 @@ const router = require('express').Router()
 const adminRouter = require('./admin.js')
 const productRouter = require('./product.js')
 const categoryRouter = require('./category.js')
-const cors = require('cors')
 
-const corsOptions = {
-  origin: 'https://ecommerce-cmsv1.firebaseapp.com',
-  optionsSuccessStatus: 200
-}
-
-router.options('*', cors(corsOptions))
 router.use('/admin', adminRouter)
 
 router.use('/product', productRouter)
