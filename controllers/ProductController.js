19b6d3626cf55c7a3f5ec84a346a88a679948c1c
adinoeleazar <adinoeleazar@gmail.com>
2020-02-21 02:09:48 +0700

    update product controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -18,6 +18,44 @@ class ProductController {
             })
             .catch(next)
     }
+
+    static update (req, res, next) {
+        const id = req.params.id
+        const { name, image_url, price, stock } = req.body
+        Product.update({ name, image_url, price, stock }, {
+            where: { id },
+            returning: true
+        })
+            .then(data => {
+                if(!data[0]) {
+                    res.status(404).json({
+                        msg: 'Data not found'
+                    })
+                }
+                else {
+                    res.status(200).json(data[1])
+                }
+            })
+            .catch(next)
+    }
+
+    static delete (req, res, next) {
+        const id = req.params.id
+        Product.destroy({ where: { id } })
+            .then(deleted => {
+                if(!deleted) {
+                    res.status(404).json({
+                        msg: 'Data not found'
+                    })
+                }
+                else {
+                    res.status(200).json({
+                        msg: 'Product has been successfully deleted'
+                    })
+                }
+            })
+            .catch(next)
+    }
 }
 
 module.exports = ProductController
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,5 +6,8 @@ const authentication = require('../middlewares/authentication')
 router.use(authentication)
 router.post('/', ProductController.create)
 router.get('/', ProductController.read)
+router.put('/:id', ProductController.update)
+router.delete('/:id', ProductController.delete)
+
 
 module.exports = router
