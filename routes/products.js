4c44e95fdec1f00c4af563d2f65ce8c71d69858f
Andara <andarasophan@gmail.com>
2020-02-20 14:56:33 +0700

    add findone product

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,7 @@ const authentication = require('../middlewares/authentication');
 router.get('/', ProductController.getProducts)
 router.use(authentication);
 router.post('/', ProductController.addProduct);
+router.get('/:productId', ProductController.findProduct);
 router.put('/:productId', ProductController.editProduct);
 router.delete('/:productId', ProductController.deleteProduct);
 
