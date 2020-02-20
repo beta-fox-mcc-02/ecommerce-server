2d8207a2f785fadc12c931466b9486942efc3526
andrumahardi <andrumahardi77@gmail.com>
2020-02-20 14:52:40 +0700

    deploy

--- app.js
+++ app.js
@@ -4,18 +4,15 @@ const indexRouter = require('./routes/index.js')
 const errHandler = require('./middlewares/errHandler.js')
 const cors = require('cors')
 
-// const corsOptions = {
-//   origin: 'http://localhost:8080',
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   preflightContinue: true,
-//   optionsSuccessStatus: 204
-// }
+app.use(cors())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
-app.use(cors())
+app.get('/', (req, res) => {
+  res.send(`audasdbasudasudasudas`)
+})
 
-app.use(indexRouter)
+// app.use(indexRouter)
 app.use(errHandler)
 
 module.exports = app
