4bd9ea2de2a5a0aeca5fd1a774f23ccbaff9fadc
Heri Susanto <susantoh41@gmail.com>
2020-02-22 15:58:50 +0700

    Ready for production

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const userRouter = require('./userRouter.js')
 const productRouter = require('./productRouter')
 
+router.get('/', res.send('Ecommerce API'))
 router.use('/users', userRouter)
 router.use('/products', productRouter)
 
