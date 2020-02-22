70ca92b6d1c2a21b4f85f867954d854ce390307f
Heri Susanto <susantoh41@gmail.com>
2020-02-22 15:50:28 +0700

    Ready for production

---
+++ seeders/20200222084637-seedUser.js
@@ -0,0 +1,39 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'admin@mail.com',
+          first_name: 'Heri',
+          last_name: 'Susanto',
+          password: '123456',
+          RoleId: 1
+        }
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+}
