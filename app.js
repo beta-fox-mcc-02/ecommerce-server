aabbd0ca8a10bbdf98b0462eede0532826398c55
haekal <haekal.yudh@gmail.com>
2020-02-22 20:36:11 +0700

    done

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-if(process.env.NODE_ENV="development"){
-  require('dotenv').config()
+if ((process.env.NODE_ENV = "development")) {
+  require("dotenv").config();
 }
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
-const routes = require('./routes')
-const errorHandler =require('./middlewares/errorHandler')
+const routes = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
 
-const logger = require('morgan')
-app.use(logger('dev'))
+const logger = require("morgan");
+app.use(logger("dev"));
 
-var cors = require('cors') 
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 
-app.use(express.json()) 
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(routes)
-app.use('/', errorHandler)
+app.use(routes);
+app.use("/", errorHandler);
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-}
+// if (process.env.NODE_ENV !== 'test') {
+//   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// }
 
-
-module.exports = app
+module.exports = app;
--- bin/http.js
+++ bin/http.js
@@ -2,8 +2,8 @@ const app = require("../app");
 const http = require("http");
 const server = http.createServer(app); // instance dari http
 // instance akan di reuse
-var cors = require("cors");
-app.use(cors());
+// var cors = require("cors");
+// app.use(cors());
 server.listen(3000, () => {
   console.log("listening on 3000");
 });
