7cc07860c7c59d411dbea5b796c9db620a1505a3
gandasipayung <gandasipayung14@gmail.com>
2020-02-22 17:57:12 +0700

    test deploy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const UserRoutes = require('./user')
 const ProductRoutes = require('./product')
 const CategoryRoutes = require('./category')
 
+router.get('/', (req, res) => {
+  res.status(200).json({
+    msg: 'Hello Gaes !'
+  })
+})
 router.use(UserRoutes)
 router.use('/products', ProductRoutes)
 router.use('/categories', CategoryRoutes)
