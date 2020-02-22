dfa3a3864698071fe41d1d223ed53809fe40067e
GanangPrakoso <ganangmanggolo@gmail.com>
2020-02-22 17:33:01 +0700

    deployyyyyy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const userRouter = require('./userRouter')
 const productRouter = require('./productRouter')
 
+router.use('/', (req, res) => {
+   res.send(`halo ganang, you're connected to server!`)
+})
 router.use('/users', userRouter)
 router.use('/products', productRouter)
 
