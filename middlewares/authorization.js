c80c4e919cfbd8c5f7501be43ee9807575c4fa92
mcc-14 <mcc-14@mcc-14s-Mac-mini.local>
2020-02-23 19:28:16 +0700

    added seeder for example products

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -2,22 +2,13 @@ const { Product } = require('../models')
 
 module.exports = function(req, res, next) {
     const id = req.params.id
-    Task.findByPk(id)
+    Product.findByPk(id)
         .then(productData => {
             if (!productData) {
                 res.status(404).json('Product not found')
             }
             else {
-                if(productData.UserId == req.currentUserId) {
-                    next()
-                }
-                else {
-                    next({
-                        name: 'AuthorizationError',
-                        code: 401,
-                        msg: 'Unauthorized User'
-                    })
-                }
+                next()
             }
         })
         .catch(next)
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const ProductController = require('../controllers/ProductController')
 const authentication = require('../middlewares/authentication')
+const authorization = require('../middlewares/authorization')
 
 router.use(authentication)
 router.post('/', ProductController.create)
 router.get('/', ProductController.read)
-router.put('/:id', ProductController.update)
-router.delete('/:id', ProductController.delete)
+router.put('/:id', authorization, ProductController.update)
+router.delete('/:id', authorization, ProductController.delete)
 
 
 module.exports = router
