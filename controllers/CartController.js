2f8ca47526fb990640ca27aadba75ba2a97aa4d0
uulwake <uulwake@example.com>
2020-02-26 12:28:06 +0700

    update

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -65,7 +65,56 @@ class CartController {
   }
 
   static update(req, res, next) {
+    const UserId = req.currentUserId;
+    const cartId = req.params.id;
+    const { ProductId, status, quantity, cartStatus } = req.body;
 
+    // check cartStatus if increment item or decrement
+
+    // check whether the quantity is enough or not
+    Product.findOne({
+      where: { id: ProductId }
+    })
+      .then(product => {
+        if (product.stock < quantity) {
+          let stock = 'stocks';
+          if (product.stock === 1) stock = 'stock';
+          next({ status: 400, message: `Only ${product.stock} ${stock} for ${product.name}` });
+        } else {
+          const data = {
+            name: product.name,
+            image_url: product.image_url,
+            price: product.price,
+            stock: product.stock - quantity
+          };
+
+          return Product.update(data, {
+            where: { id: ProductId },
+            returning: true,
+            plain: true
+          });
+        }
+      })
+      .then(product => {
+        const data = {
+          UserId,
+          ProductId,
+          status,
+          quantity,
+          price: quantity * product[1].price
+        };
+
+        // update cart
+        return Cart.update(data, {
+          where: { id: cartId },
+          returning: true
+        });
+      })
+      .then(cart => {
+        // send success status
+        res.status(200).json({ message: `Success Update Item` });
+      })
+      .catch(next);
   }
 }
 
