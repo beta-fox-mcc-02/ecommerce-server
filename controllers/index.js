bb77b419d49ba8cb1fcc25fa9a2e1322922f22a9
Ahmad Muhammad Satria Adiputra <49425792+ninoloid@users.noreply.github.com>
2020-02-27 03:22:20 +0700

    Revert "Development"

--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-const userController = require('./userController')
-const productController = require('./productController')
-const cartController = require('./cartController')
-const discountController = require('./discountController')
-
-module.exports = { userController, productController, cartController, discountController }
--- middlewares/checkStock.js
+++
@@ -1,5 +0,0 @@
-const { Product, Cart } = require('../models')
-
-module.exports = (req, res, next) => {
-  
-}
