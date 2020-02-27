1b2eed7c5b43597567ef5eecedb8d2b8c4ec4543
Andara <andarasophan@gmail.com>
2020-02-27 22:10:48 +0700

    deploy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const peopleRouter = require('./people');
 const productRouter = require('./products');
 const cartRouter = require('./carts');
 
+router.get('/', (req, res) => {
+  res.send('hello world');
+});
 router.use('/', peopleRouter);
 router.use('/products', productRouter)
 router.use('/carts', cartRouter)
