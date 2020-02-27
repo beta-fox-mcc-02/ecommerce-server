173fc97079a2ca5c1a7a729123674c97c907b6bf
Andara <andarasophan@gmail.com>
2020-02-27 12:59:57 +0700

    timeout

--- controllers/carts.js
+++ controllers/carts.js
@@ -1,4 +1,4 @@
-const { Cart } = require('../models/index');
+const { Cart, Product } = require('../models/index');
 
 class Controller {
   static createCart(req, res, next) {
@@ -56,6 +56,94 @@ class Controller {
         } else {
           res.status(200).json(response);
         }
+      })
+      .catch(err => {
+        next(err);
+      })
+  }
+  static deleteCart(req, res, next) {
+    Cart.destroy({
+      where: {
+        PersonId: req.params.personId,
+        ProductId: req.params.productId,
+      }
+    })
+    .then(response => {
+      if (response) {
+        res.status(200).json(response);
+      } else {
+        next({
+          message: 'Cart not found',
+          status: 404
+        })
+      }
+    })
+    .catch(err => {
+      next(err);
+    })
+  }
+  static checkoutCart(req, res, next) {
+    let paidQuantity;
+    Cart.findOne({
+      where: {
+        PersonId: req.params.personId,
+        ProductId: req.params.productId,
+      }
+    })
+      .then(response => {
+        if (response) {
+          paidQuantity = response.quantity;
+          let update = {
+            PersonId: response.PersonId,
+            ProductId: response.ProductId,
+            price: response.price,
+            quantity: response.quantity,
+            paid: true,
+          }
+          return Cart.update(update, {
+            where: {
+              PersonId: req.params.personId,
+              ProductId: req.params.productId,
+            },
+            returning: true,
+          })
+        } else {
+          next({
+            message: 'Product not found',
+            status: 404
+          })
+        }
+      })
+      .then(response => {
+        if (response) {
+          return Product.findOne({
+            where: {
+              id: req.params.productId, 
+            },
+          })
+        } else {
+          next({
+            message: 'Cart not found',
+            status: 404
+          })
+        }
+      })
+      .then(response => {
+        let updateStock = response.stock - paidQuantity;
+        let update = {
+          name: response.name,
+          image_url: response.image_url,
+          price: response.price,
+          stock: updateStock,
+        }
+        return Product.update(update, {
+          where: {
+            id: req.params.productId,
+          }
+        })
+      })
+      .then(response => {
+        res.status(200).json(response);
       })
       .catch(err => {
         next(err);
--- routes/carts.js
+++ routes/carts.js
@@ -6,5 +6,7 @@ router.get('/:personId/:productId', CartController.findCart);
 router.use(authentication);
 router.post('/', CartController.createCart);
 router.put('/:personId/:productId', CartController.editCart);
+router.delete('/:personId/:productId', CartController.deleteCart);
+router.put('/:personId/:productId/checkout', CartController.checkoutCart);
 
 module.exports = router;
