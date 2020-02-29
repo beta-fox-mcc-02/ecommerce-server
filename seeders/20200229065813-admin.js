e41b399856e19b4ee1fa8fe8c50d5b87b5252262
orionslg <solang.orion@gmail.com>
2020-02-29 14:00:25 +0700

    added seeders

---
+++ seeders/20200229065813-admin.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+        return queryInterface.bulkInsert('Users', [{
+          username: 'admin',
+          email: 'admin@gmail.com',
+          password: '123456',
+          roles: 'admin'
+        }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+     return queryInterface.bulkDelete('Users', null, {});
+  }
+};
